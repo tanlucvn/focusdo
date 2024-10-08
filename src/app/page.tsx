3e5d1f9abf3c8ad5976da16eb5bf250099e12a42
tanluc1e <t.luc497303@gmail.com>
2024-10-08 12:05:37 +0700

    feat(ui): add homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image"
-
-import { Button } from "@/components/ui/button"
+import HomePage from "@/pages/home-page";
 
 export default function Home() {
-  return <Button>Hello world</Button>
+  return <HomePage />;
 }
---
+++ src/components/app-tabs.tsx
@@ -0,0 +1,37 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { LayoutList, ListChecks, PieChartIcon, TrashIcon } from "lucide-react";
+
+const AppTabs = () => {
+  return (
+    <Tabs defaultValue="todos">
+      <TabsList className="grid w-full grid-cols-4 rounded-full">
+        <TabsTrigger value="todos" className="flex gap-2 rounded-full">
+          <LayoutList size={16} />
+        </TabsTrigger>
+        <TabsTrigger value="completed" className="flex gap-2 rounded-full">
+          <ListChecks size={16} />
+        </TabsTrigger>
+        <TabsTrigger value="trash" className="flex gap-2 rounded-full">
+          <TrashIcon size={16} />
+        </TabsTrigger>
+        <TabsTrigger value="progress" className="flex gap-2 rounded-full">
+          <PieChartIcon size={16} />
+        </TabsTrigger>
+        <TabsContent value="todos" className="col-span-full">
+          Todos
+        </TabsContent>
+        <TabsContent value="completed" className="col-span-full">
+          Completed
+        </TabsContent>
+        <TabsContent value="trash" className="col-span-full">
+          Trash
+        </TabsContent>
+        <TabsContent value="progress" className="col-span-full">
+          Progress
+        </TabsContent>
+      </TabsList>
+    </Tabs>
+  );
+};
+
+export default AppTabs;
