8c55f6b669342bde800d5f0f76233d521cc141a9
tanluc1e <t.luc497303@gmail.com>
2024-10-31 12:45:16 +0700

    style: add inter font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
